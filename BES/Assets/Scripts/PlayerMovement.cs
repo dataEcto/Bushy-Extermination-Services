2c8f31910a31d74d1686ea38da1b51746c777d41
Genric Jan Elazegui <gj.elazegui@Gmail.com>
2018-07-02 11:31:19 -0400

    First tasked completed, second task halfway
    Movement is done, but I will eventually tweak it. Shield stuff is being done as well. The hard part will obviously be the reflection properties.

---
+++ BES/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody2D player_rb;
+	public float speed;
+
+	//Health Bar Variables
+	public Slider healthBar;
+	public float currentHealth { get; set; }
+	public float maxHealth { get; set; }
+	public TextMeshProUGUI healthBarText;
+
+	void Start ()
+	{
+		player_rb = GetComponent<Rigidbody2D>();
+
+		//can be any value of course
+		maxHealth = 100f;
+
+		//This is to reset the value of the health bar to full health every time the game is loaded.
+		currentHealth = maxHealth;
+
+		///Visual representation of the health.
+		//Get the value of the slider 
+		//set it to calculate health
+		healthBar.value = CalculateHealth();
+
+		//Display the intial health
+		healthBarText.text = "Health: " + currentHealth;
+
+	}
+	
+	
+	void Update ()
+	{
+		player_rb.velocity = new Vector2(0, 0);
+
+		//Movement
+		if (Input.GetKey(KeyCode.W))
+		{
+			player_rb.velocity += new Vector2(0, speed);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			player_rb.velocity += new Vector2(0, -speed);
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			player_rb.velocity += new Vector2(speed, 0);
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			player_rb.velocity += new Vector2(-speed, 0);
+		}
+
+		//Displays the CURRENT health
+		healthBarText.text = "Health: " + Mathf.Round(currentHealth);
+
+		//Test Damage Input
+		//Again, the player won't actually take damage when space bar is pressed.
+		if (Input.GetKey(KeyCode.Space))
+		{
+			DealDamage(5);
+		}
+	}
+
+	//The function that calculates what the health is currently.
+	//What it does is take the health at the moment, then divide it by max health
+	float CalculateHealth()
+	{
+		return currentHealth / maxHealth;
+	}
+
+
+	//A temporary function used just to check if damage is being dealt to the player
+	//This will be deleted later, or perhaps changed so that when the player collides with an enemy/projectile,
+	//This function will be called.
+	void DealDamage(float damageValue)
+	{
+
+		//Deal damage to the health bar
+		currentHealth -= damageValue;
+		//Same as from start
+		healthBar.value = CalculateHealth();
+
+		//If the character is out of health, they die
+		if (currentHealth <= 0)
+		{
+			//This displays the health to be 0.
+			//This is to prevent negative numbers to show up
+			healthBarText.text = "Health: " + 0;
+			print("You Died");
+			Destroy(gameObject);
+		}
+
+	}
+}
